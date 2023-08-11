196e6bccaad9b4fc2afe75cc5aa98c4dc5acfe75
Bastien Sun <43788700+bastiensun@users.noreply.github.com>
2023-08-11 21:14:31 +0200

    fix: pass in an i18next instance to fix i18n issue
    cf. https://github.com/i18next/next-i18next/blob/master/TROUBLESHOOT.md#need-to-pass-in-an-i18next-instance

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,16 @@
 import CallBob from './CallBob'
+import {GetStaticProps} from "next";
+import {Props} from "next/script";
+import {getServerTranslations} from "@/lib/i18n/getServerTranslations";
+
+export const getStaticProps: GetStaticProps<Props> = async (context) => {
+  const { locale } = context;
+  return {
+    props: {
+      ...(await getServerTranslations(locale, ['common'])),
+    },
+  };
+};
 
 export default function Home() {
   return <CallBob />
