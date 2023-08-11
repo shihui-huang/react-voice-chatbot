9c49a2255033d70e67329e4741425c871dce4996
Bastien Sun <43788700+bastiensun@users.noreply.github.com>
2023-08-11 21:14:31 +0200

    fix: pass in an i18next instance to fix i18n issue
    cf. https://github.com/i18next/next-i18next/blob/master/TROUBLESHOOT.md#need-to-pass-in-an-i18next-instance

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,10 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { appWithTranslation } from 'next-i18next'
+import nextI18NextConfig from '../next-i18next.config'
 
-export default function App({ Component, pageProps }: AppProps) {
+function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
 }
+
+export default appWithTranslation(App, nextI18NextConfig)
